731da5d8d33acd4c6bff68b6563b984af916b600
Prathik Raj <prathik011@gmail.com>
2018-04-28 23:17:37 +0530

    This is where it all begins...

--- fcm/cli.py
+++ fcm/cli.py
@@ -2,16 +2,18 @@ import click
 from message_loaders import LocalMessageLoader
 from message_loaders import LocalFunnyLoader
 from message_loaders import LocalHackLoader
+from signatures import get_signature
 import git
 import os
 
 @click.command()
 @click.option('--first', '-f', is_flag=True, help='Interesting first commit message')
 @click.option('--hack', '-h', is_flag=True, help='Adds a joke that the commit is a hack')
+@click.option('--signature', '-s', help='Adds an ascii signature to the commit')
 @click.option('--message', '-m', help="Commit message to be appended with")
-def main(first, hack, message):
+def main(first, hack, signature, message):
     """Generates funny commit message for you"""
-    loader = LocalMessageLoader() if first else LocalFunnyLoader()
+    
     if first:
         loader = LocalMessageLoader()
     elif hack:
@@ -21,10 +23,17 @@ def main(first, hack, message):
 
     cm = loader.get_message()
 
+    signatures = {
+        ""
+    }
+
     if message:
         cm = """{}
 ===
 {}""".format(message, cm)
+
+    if signature:
+        print get_signature(signature)
         
     repo = git.Repo(os.getcwd())
     repo.git.commit('-am', cm)
